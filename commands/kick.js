724b5bba242a464255bfdd3868622d430303fd1d
Pesky12 <alesoelsov@gmail.com>
2018-02-01 11:41:56 +0100

    Rewrite kick

--- commands/kick.js
+++ commands/kick.js
@@ -1,51 +1,17 @@
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
-  let reason = args.slice(1).join(' ');
-  let userToBan = await message.mentions.users.first() || client.users.get(args[0]);
+  let usersToBan = message.mentions.users
+  if (usersToBan.size < 1) return message.channel.send('Can you mention them ?')
+  let reason = args.slice(usersToBan.array().length).join(' ') || 'There is none! ¯\\_(ツ)_/¯'
 
-  let errorEmbed = new Discord.RichEmbed()
-    .setAuthor("Missing arguments!!")
-    .setDescription("kick [mention/userID] [reason]")
-    .setColor('#f22a0c');
-
-  if(!userToBan || !reason){
-    message.channel.send(errorEmbed)
-    return
-  }
-
-  if (!message.guild.member(userToBan).kickable) {
-    let embed = new Discord.RichEmbed()
-      .setTitle('This user is not kickable for me!')
-      .setColor('#f22a0c')
-
-    message.channel.send({embed}).then(botmsg => {botmsg.delete(5000)});
-    return;
-  }
-  
-  message.guild.member(userToBan).kick();
-  message.delete()
-
-  let embedModLog = new Discord.RichEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setColor(0x00AE86)
-    .setTimestamp(new Date)
-    .addField('Action', 'Kick', true)
-    .addField('Moderator', message.author.tag, true)
-    .addField('Target', userToBan.tag, true)
-    .addField('Reason', reason, true)
-    .setFooter(message.guild.name, message.guild.iconURL);
-
-  let embedSmall = new Discord.RichEmbed()
-    .setAuthor(`${userToBan.tag} has been kicked out! 👌 `, userToBan.displayAvatarURL)
-    .setDescription('Hes gone for now...')
-    .setColor('#00ff0c'); 
-
-
-
-  if(message.guild.channels.find('name', 'mod-log')){ message.guild.channels.find('name', 'mod-log').send({embed:embedModLog}) }
-  message.channel.send({ embed: embedSmall })
-  client.users.get(userToBan.id).send({embed:embedModLog})
-};
+  usersToBan.map(u => {
+    if (!message.guild.members.find('id', u.id).bannable) return message.channel.send(randomMessages.botCant(u, 'ban'))
+    let em = modLogEmbed.modActionEmbed('Ban', message.author, u, reason)
+    if (message.guild.channels.find('name', 'mod-log')) message.guild.channels.find('name', 'mod-log').send({embed: em})
+    message.channel.send(randomMessages.ban(message.author, u))
+    message.guild.ban(u, reason)
+  })
+}
 
 exports.settings = {
   enabled: true,     
