686f7101462f80ebebe80e862bf755bb38bd1046
Pesky12 <alesoelsov@gmail.com>
2018-02-04 23:16:19 +0100

    Added logging of changed values

--- events/channelUpdated.js
+++ events/channelUpdated.js
@@ -1,33 +1,35 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-exports.run = (client, config) =>{
-  client.on('channelUpdate', (oldChannel, newChannel) => {
-    if(newChannel.type == 'dm')
-      return;
-    if(oldChannel.guild.channels.find('name', 'mod-log')) {
-      let embed = new Discord.RichEmbed()
-        .setAuthor(`Channel #${oldChannel.name} | #${newChannel.name} has been updated!`)
-        .setDescription('For more info check the audit log')
-        .addField('Type', `${oldChannel.type }`, true)
-        .addField('ID', `${oldChannel.id}`, true)
+exports.run = (client, config) => {
+  client.on('channelUpdate', async(oldChannel, newChannel) => {
+    if (!newChannel.guild.channels.exists('name', 'mod-log')) return
+    let auditLog = await oldChannel.guild.fetchAuditLogs({ limit: 1, type: 11 })
+    let auditLogEntry = auditLog.entries.first()
+    let oldValues = []
+    let newValues = []
+    auditLogEntry.changes.forEach(change => {
+      if (change.key === 'permission_overwrites') return
+      newValues.push(`${change.key}: ${change.new}`)
+      oldValues.push(`${change.key}: ${change.old}`)
+    })
+    let embed = new Discord.RichEmbed()
+        .setAuthor(`Channel #${oldChannel.name} has been modified!`)
+        .addField('Moderator', auditLogEntry.executor)
+        .addField('Changes', 'ᅠ', true)
+        .addField('Old values', oldValues, true)
+        .addField('New Values', newValues, true)
         .setColor('#c4350d')
-        .setFooter('Channel', client.user.avatarURL)
-        .setTimestamp(new Date());
-      oldChannel.guild.channels.find('name', 'mod-log').send({ embed });
-    }
-    else {
-      return;
-    }
-  });
-};
+        .setTimestamp(new Date())
+    oldChannel.guild.channels.find('name', 'mod-log').send({ embed })
+  })
+}
 
 exports.help = {
-  name:"Channel Updated",
+  name: 'Channel Updated',
   description: "Triggered when channel's perms, name, etc has been chanaged"
 }
 
-
 exports.settings = {
-    enabled: false,
-    public: true,
-  };
+  enabled: true,
+  public: true
+}
--- events/deletedGuildChannel.js
+++ events/deletedGuildChannel.js
@@ -1,31 +1,31 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-exports.run = (client, config) =>{
-  client.on('channelDelete', (channel) => {
-    if(channel.type == 'dm')
-      return;
-    if(channel.guild.channels.find('name', 'mod-log')) {
-      let embed = new Discord.RichEmbed()
+exports.run = (client, config) => {
+  client.on('channelDelete', async(channel) => {
+    if (!channel.guild.channels.exists('name', 'mod-log')) return
+    let auditLog = await channel.guild.fetchAuditLogs({ limit: 1, type: 12 })
+    let auditLogEntry = auditLog.entries.first()
+    let oldValues = []
+    auditLogEntry.changes.forEach(change => {
+      if (change.key === 'permission_overwrites') return
+      oldValues.push(`${change.key}: ${change.old}`)
+    })
+    let embed = new Discord.RichEmbed()
         .setAuthor(`Channel #${channel.name}(${channel.id}) has been deleted!`)
-        .setDescription('For more info check the audit log')
-        .addField('Type', `${channel.type }`)
+        .addField('Old values', oldValues, true)
+        .addField('Moderator', `${auditLogEntry.executor}`, true)
         .setColor('#c4350d')
-        .setFooter('Channel', client.user.avatarURL)
-        .setTimestamp(new Date());
-      channel.guild.channels.find('name', 'mod-log').send({ embed });
-    }
-    else {
-      return;
-    }
-  });
-};
+        .setTimestamp(new Date())
+    channel.guild.channels.find('name', 'mod-log').send({ embed })
+  })
+}
 
 exports.help = {
-  name:"Channel Deleted",
-  description: "Triggered when channel is deleted"
+  name: 'Channel Deleted',
+  description: 'Triggered when channel is deleted'
 }
 
 exports.settings = {
-      enabled: true,     
-      public: true,
-};
+  enabled: true,
+  public: true
+}
