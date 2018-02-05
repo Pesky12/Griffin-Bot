8d615e666e6f0b4e113de545074f0b96f06bb50e
Pesky12 <alesoelsov@gmail.com>
2018-02-05 12:46:30 +0100

    Removed throwError

--- commands/achivmentmeme.js
+++ commands/achivmentmeme.js
@@ -1,6 +1,5 @@
 const Jimp = require('jimp')
 const awaitInput = require('../Utils/inputAway')
-const throwError = require('../Utils/throwError')
 
 exports.run = async(client, message, args) => {
   let rip = args.join(' ')
@@ -9,22 +8,16 @@ exports.run = async(client, message, args) => {
   if (args.length < 1) {
     rip = await awaitInput.run(message.channel, 8000, 1, m => m.author.id === message.author.id, 'What is the name of the achievment?')
     rip = await rip.first().content
-    console.log(rip)
-  }
-  if (!rip) {
-    throwError.throwEmbed('I need something to write on there.')
-    return
   }
 
   message.channel.startTyping()
-  Jimp.read(achievURL, function (err, image) {
-    Jimp.loadFont(Jimp.FONT_SANS_16_WHITE).then(function (font) {
+  Jimp.read(achievURL, (err, image) => {
+    Jimp.loadFont(Jimp.FONT_SANS_16_WHITE).then(font => {
       image.print(font, 59, 32, rip)
       image.write(`../img/achiv${message.author.id}.png`, () => {
         message.channel.send({file: `../img/achiv${message.author.id}.png`})
         message.channel.stopTyping()
-      }
-      )
+      })
     })
     if (err) throw err
   })
