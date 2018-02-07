4faa4ae92f81b2504012956db191d74bb001cd2b
Pesky12 <alesoelsov@gmail.com>
2018-02-07 22:00:17 +0100

    Added multiple currency support

--- commands/doge.js
+++ commands/doge.js
@@ -1,37 +1,29 @@
-const got = require('got');
-const Discord = require('discord.js')
+const got = require('got')
+const embeds = require('../Utils/embeds')
 
 exports.run = (client, message, args, config) => {
-  got(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=DOGE&tsyms=USD`, { json: true }).then(response => {
-    // 396416921030885379
-    message.channel.send({ 
-      embed: new Discord.RichEmbed()
-      .setAuthor(response.body['DISPLAY']['DOGE']['USD']['FROMSYMBOL'], 'http://hoekomikaangeld.com/wp-content/uploads/2015/12/dogecoin-logo.png')
-      .setThumbnail('https://altcoins.com.au/img/logo-lg.png')
-      .addField('Price (USD)', response.body['DISPLAY']['DOGE']['USD']['PRICE'])
-      .addField('Today\'s high', response.body['DISPLAY']['DOGE']['USD']['HIGHDAY'], true)
-      .addField('Today\s low', response.body['DISPLAY']['DOGE']['USD']['LOWDAY'], true)
-      .setColor('#FFDF00')
-      .setFooter('cryptocompare.com', 'https://freeter.io/embedding-web-apps/cryptocurrency/cryptocompare.png')
-      .setTimestamp(new Date())
-    })
-  }).catch(error => {
-    console.log(error);
-  });
-};
+  args = args.join(' ').split(' to ')
+  let crypto = args[0].toUpperCase() || 'DOGE'
+  let convertTo = args[1].toUpperCase() || 'USD'
+  got(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${crypto}&tsyms=${convertTo}`).then(response => {
+    response = JSON.parse(response.body)
+    response = response['DISPLAY'][crypto || convertTo][convertTo || crypto]
+    console.log(response)
+    message.channel.send({ embed: embeds.currencyEmbed(response) })
+  }).catch(console.log)
+}
 
 exports.settings = {
-  enabled: true,     
+  enabled: true,
   public: true,
   PM: true,
   owneronly: false,
-  permissionsRequired: [],
-};
+  permissionsRequired: []
+}
 
 exports.help = {
-  name: 'doge',
+  name: 'crypto',
   description: '',
-  longDescription: "",
+  longDescription: '',
   usage: ''
-};
-
+}
