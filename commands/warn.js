a9d69ab4a60609f599e0f754abe913a316a24a5c
Pesky12 <alesoelsov@gmail.com>
2018-02-08 12:51:57 +0100

    Refractioning

--- commands/warn.js
+++ commands/warn.js
@@ -1,93 +1,27 @@
-const Discord = require('discord.js');
+const embeds = require('../Utils/embeds')
+const random = require('../Utils/randomSelector')
 exports.run = (client, message, args) => {
-  let reason = args.slice(1).join(' ');
-  let user = message.mentions.users.first();
-  if (reason.length < 1){
-    let embed = new Discord.RichEmbed()
-      .setTitle('Specify a reason for warning!')
-      .setColor('#f22a0c')
-      .setTimestamp(new Date());
-
-    message.channel.send({embed});
-    return;
-  }
-  if (message.mentions.users.size < 1) {
-    let embed = new Discord.RichEmbed()
-      .setTitle('Please specify any mentions.')
-      .setColor('#f22a0c')
-      .setTimestamp(new Date());
-
-    message.channel.send({embed}).then(msg => msg.delete());
-    return;
-  }
-  message.delete();
-  if(message.guild.channels.find('name', 'mod-log')){
-    const embedChat = new Discord.RichEmbed()
-      .setAuthor(`${message.author.username} used "WARNING LETTER"!`, message.author.avatarURL)
-      .setColor('#ff5d00')
-      .setTimestamp(new Date)
-      .addField(`${user.tag} has been warned!`, 'Hes should get a PM with more info!\nCheck mod-log for more info.', true)
-      .setFooter('Warn', client.user.avatarURL);
-    const embed = new Discord.RichEmbed()
-      .setDescription('Copy of this message was sent to #mod-log and the warned user!')
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor('#ff5d00')
-      .setTimestamp(new Date())
-      .addField('Action', 'Warning', true)
-      .addField('Moderator', `${message.author.tag}`, true)
-      .addField('Target', `${user.tag}`, true)
-      .addField('Reason', `${reason}`, false)
-      .setFooter('Warn', client.user.avatarURL);
-    const embedUser = new Discord.RichEmbed()
-      .setDescription('YOU HAVE BEEN WARNED!!')
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor('#ff5d00')
-      .setTimestamp(new Date())
-      .addField('Action', 'Warning', true)
-      .addField('Moderator', `${message.author.tag}`, true)
-      .addField('Target', `${user.tag}`, true)
-      .addField('Reason', `${reason}`, false)
-      .setFooter('Warn', client.user.avatarURL);
-    message.channel.send({embed: embedChat});
-    message.guild.channels.find('name', 'mod-log').send({embed});
-    client.users.get(user.id).send({embed: embedUser});
-  } else {
-    const embed = new Discord.RichEmbed()
-      .setDescription('Copy of this message was sent to owner and the warned user!\nCreating a #mod-log channel is recomended!')
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor('#ff5d00')
-      .setTimestamp(new Date())
-      .addField('Action', 'Warning', true)
-      .addField('Moderator', `${message.author.tag}`, true)
-      .addField('Target', `${user.tag}`, true)
-      .addField('Reason', `${reason}`, false)
-      .setFooter('Warn', client.user.avatarURL);
-    const embedUser = new Discord.RichEmbed()
-      .setDescription('YOU HAVE BEEN WARNED!!')
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setColor('#ff5d00')
-      .setTimestamp(new Date())
-      .addField('Action', 'Warning', true)
-      .addField('Moderator', `${message.author.tag}`, true)
-      .addField('Target', `${user.tag}`, true)
-      .addField('Reason', `${reason}`, false)
-      .setFooter('Warn', client.user.avatarURL);
-    message.channel.send({embed});
-    client.users.get(user.id).send({embed: embedUser});
-  }
-};
+  let warnedUsers = message.mentions.users
+  let reason = args.slice(warnedUsers.array().length).join(' ') || 'There is none! ¯\\_(ツ)_/¯'
+  warnedUsers.map(u => {
+    let warnEmbed = embeds.modActionEmbed('Warn', message.author, u, reason)
+    if (message.guild.channels.exists('name', 'mod-log')) message.guild.channels.find('name', 'mod-log').send({embed: warnEmbed})
+    u.send({ embeds: warnEmbed })
+  })
+  message.channel.send(random.warn(warnedUsers.map(u => { return u })))
+}
 
 exports.settings = {
-  enabled: true,     
+  enabled: true,
   public: true,
   PM: false,
   owneronly: false,
-  permissionsRequired: ['MANAGE_MESSAGES'],
-};
+  permissionsRequired: ['MANAGE_MESSAGES']
+}
 
 exports.help = {
   name: 'warn',
   description: '🛑 Issues a warning to the mentioned user.',
-  longDescription: "",
+  longDescription: '',
   usage: 'warn [mention] [reason]'
-};
+}
