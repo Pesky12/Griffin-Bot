87c3778e3aa916811200ee3ff6d2bde39c90b463
Pesky12 <alesoelsov@gmail.com>
2018-02-11 23:38:51 +0100

    Linting & changing to not public

--- commands/ping.js
+++ commands/ping.js
@@ -1,21 +1,18 @@
-exports.run = (client, message, args,config) => {
-    message.channel.send('Ping?!')
-      .then(msg => {
-        msg.edit(`<:gun:333359555117580291> BANG! Ur dead! (Took me: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
-      });
-};
+exports.run = (client, message, args, config) => {
+  message.channel.send('Ping?!').then(msg => { msg.edit(`<:gun:333359555117580291> BANG! Ur dead! (Took me: ${msg.createdTimestamp - message.createdTimestamp}ms)`) })
+}
 
 exports.settings = {
-  enabled: true,     
-  public: true,
+  enabled: true,
+  public: false,
   PM: true,
   owneronly: false,
-  permissionsRequired: [],
-};
+  permissionsRequired: []
+}
 
 exports.help = {
   name: 'ping',
   description: '🏓 I wonder what it does.',
-  longDescription: "",
+  longDescription: '',
   usage: 'ping'
-};
+}
