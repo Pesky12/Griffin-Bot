ad7848ff3d91798fe5bae0cb20fe1d93de4b00d9
Pesky12 <alesoelsov@gmail.com>
2018-02-12 08:35:33 +0100

    Linting

--- commands/rip.js
+++ commands/rip.js
@@ -1,36 +1,31 @@
-const Jimp = require('jimp');
+const Jimp = require('jimp')
 
-exports.run = (client, message, args) =>{
-  message.channel.startTyping();
-  Jimp.read('http://tombgen.appspot.com/images/tombstone.png', function(err, image) {
-    let rip = args.join(' ');
-    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function(font) {
-      image.print(font, 143, 107, rip, 400);
-      image.write(`../img/rip${message.author.id}.png`, function() {
+exports.run = (client, message, args) => {
+  message.channel.startTyping()
+  let rip = args.join(' ')
+  Jimp.read('http://tombgen.appspot.com/images/tombstone.png', function (err, image) {
+    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (font) {
+      image.print(font, 143, 107, rip, 400)
+      image.write(`../img/rip${message.author.id}.png`, function () {
         message.channel.send({file: `../img/rip${message.author.id}.png`})
-        message.channel.stopTyping()
-       });
-  
-    });
-    if(err)
-      throw err;
-  });
-
-
-};
+      })
+    })
+    if (err) { throw err }
+  })
+  message.channel.stopTyping()
+}
 
 exports.settings = {
-  enabled: true,     
+  enabled: true,
   public: true,
   PM: true,
   owneronly: false,
-  permissionsRequired: [],
-};
+  permissionsRequired: []
+}
 
 exports.help = {
   name: 'rip',
   description: '🗿 Creates a tombstone with a defined text.',
-  longDescription: "",
+  longDescription: '',
   usage: 'rip [text]'
-};
-
+}
