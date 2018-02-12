d446ced3c1ffbadc98581a752d5bc036f2ba8d4b
Pesky12 <alesoelsov@gmail.com>
2018-02-12 08:34:34 +0100

    Made to reload any require

--- commands/reload.js
+++ commands/reload.js
@@ -1,20 +1,20 @@
-exports.run = (client, message, args,config) => {
-  if (!args || args.size < 1) return message.reply("Must provide a command name to reload.");
-  delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.reply(`The command ${args[0]} has been reloaded`);
-};
+exports.run = (client, message, args, config) => {
+  if (!args || args.size < 1) return message.reply('Must provide a command name to reload.')
+  delete require.cache[require.resolve(args)]
+  message.reply(`The command ${args[0]} has been reloaded`)
+}
 
 exports.settings = {
-  enabled: true,     
+  enabled: true,
   public: false,
   PM: true,
   owneronly: true,
-  permissionsRequired: [],
-};
+  permissionsRequired: []
+}
 
 exports.help = {
   name: 'reload',
   description: '',
-  longDescription: "",
+  longDescription: '',
   usage: ''
-};
+}
