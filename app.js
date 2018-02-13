bdd21cf19f209557a7ac275aab9924f88e90b49b
Pesky12 <alesoelsov@gmail.com>
2018-02-13 13:55:52 +0100

    Added checking for owner only commands

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
 const dotenv = require('dotenv')
-const config = require('./config.json')
 
-if (process.env.RUN_TYPE !== 'production') {
-  dotenv.load()
-}
+if (process.env.RUN_TYPE !== 'production') dotenv.load()
 
 client.commands = new Discord.Collection()
 client.events = new Discord.Collection()
@@ -23,7 +20,7 @@ function loader (loadFolder, collection, requiring) {
     filesjs.forEach((f, i) => {
       let file = require(`${loadFolder}${f}`)
       if (file.settings.enabled === false) return console.log(`${i + 1}: ${f} is disabled and will not be loaded`)
-      if (requiring) file.run(client, config)
+      if (requiring) file.run(client)
       console.log(`${i + 1}: ${f} ready to fly!`)
       collection.set(file.help.name, file)
     })
@@ -35,18 +32,18 @@ loader('./commands/', client.commands)
 loader('./events/', client.events, true)
 
 client.on('message', (message) => {
-  let prefix = config.prefix
-  if (!message.content.startsWith(prefix)) return
-  if (message.author.bot) return
-  let messageAray = message.content.split(' ')
-  let command = messageAray[0]
-  let args = messageAray.slice(1)
-  let cmd = client.commands.get(command.slice(prefix.length))
+  let prefix = process.env.prefix
+  let messageHandle = message.content.split(' ')
+  let args = messageHandle.slice(1)
+  let commandName = messageHandle[0].slice(prefix.length)
+  let command = client.commands.get(commandName)
 
-  if (cmd) {
-    if (cmd.settings.permissionsRequired[0] && !message.guild.member(message.author).hasPermission(cmd.settings.permissionsRequired[0])) return message.channel.send('You don\'t have perms.')
-    if (cmd.settings.PM === false & message.channel.type !== 'text') return message.channel.send('This command is not allowed in PMs!!')
-    cmd.run(client, message, args, config)
+  if (command) {
+    if (command.settings.permissionsRequired[0] && !message.guild.member(message.author).hasPermission(command.settings.permissionsRequired[0])) return message.channel.send('You don\'t have perms.')
+    if (command.settings.PM === false & message.channel.type !== 'text') return message.channel.send('This command is not allowed in PMs!!')
+    if (command.settings.owneronly && message.author.id !== process.env.OWNER_ID) return
+    command.run(client, message, args)
+    console.log(`${message.author.tag} used ${commandName}`)
   }
 })
 
