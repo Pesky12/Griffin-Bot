610905cc7b07290136337754d40692418f91aa3c
Pesky12 <alesoelsov@gmail.com>
2018-02-28 18:34:51 +0100

    Adding sample test

---
+++ test/checkAccess.test.js
@@ -0,0 +1,11 @@
+describe('checkPerms', () => {
+  it('should return True if command is public and does not require any perms.', () => {
+    let settings = {
+      enabled: true,
+      public: true,
+      PM: false,
+      owneronly: false,
+      permissionsRequired: []
+    }
+  })
+})
