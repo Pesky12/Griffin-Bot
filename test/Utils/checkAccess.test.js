01a3ce81138bc86214198c26eedc28948d44a047
Pesky12 <alesoelsov@gmail.com>
2018-03-13 17:59:06 +0100

    Removed firebase unit testing

--- test/Utils/checkAccess.test.js
+++ test/Utils/checkAccess.test.js
@@ -2,32 +2,6 @@ import { checkPerms, checkCommandPerms, getSettings, getGuildSettings, addDefaul
 import { mockUser, mockGuildMember, mockChannel } from '../../lib/Utils/mockData';
 import { Permissions } from "discord.js"
 import { use, expect } from 'chai'
-import firebase from 'firebase-admin'
-import firebaseAccount from '../../firebase'
-import chaiPromise from 'chai-as-promised'
-
-use(chaiPromise)
-process.env.OWNER_ID = 235047463017381888
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(firebaseAccount),
-  databaseURL: `https://${firebaseAccount.project_id}.firebaseio.com/`
-})
-
-describe('addDefaultData', async() => {
-})
-
-describe('getSettingsGuild', () => {
-  it('Should return an object', () => {
-    expect(getGuildSettings('404434706843566092')).to.eventually.be.an('Object')
-  })
-})
-
-describe('getSettings', () => {
-  it('Returns an object', async() => {
-    expect(getSettings('404434706843566092', 'help')).to.eventually.be.an('Object')
-  })
-})
 
 describe('checkCommandPerms', () => {
   const cmdNormal = require('../../lib/commands/help')
