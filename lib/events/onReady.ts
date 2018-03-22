eb205ed5a918c8ccc009a322deefedf3bad338b7
Pesky12 <alesoelsov@gmail.com>
2018-03-22 19:07:32 +0100

    • ↄ •

--- lib/events/onReady.ts
+++ lib/events/onReady.ts
@@ -1,6 +1,6 @@
 exports.run = (client) => {
   client.on('ready', () => {
-    console.log(`${client.user.tag} is up in ${client.guilds.size} guilds, for ${client.users.size} users!\nFlight started at ${new Date()}\nUsing Gbot by Pesky12!\nTook: ${process.uptime()} seconds!`)
+    console.log(`${client.user.tag} is up in ${client.guilds.size} guilds, for ${client.users.size} users! • ↄ •\nFlight started at ${new Date()}\nUsing Gbot by Pesky12!\nTook: ${process.uptime()} seconds!`)
     client.user.setPresence({ game: { name: `use ${process.env.PREFIX}help | Serving in: ${client.guilds.size} guilds!`, type: 0 } })
     client.channels.get(process.env.LOG_CHANNEL).send(`Bot started in ${client.guilds.size} guilds`)
   })
