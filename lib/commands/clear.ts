be94e9ae79a6eb4c44050a602d7b9ddbb1014e21
Pesky12 <alesoelsov@gmail.com>
2018-03-27 12:52:38 +0200

    Fixed #6

--- lib/commands/clear.ts
+++ lib/commands/clear.ts
@@ -1,10 +1,12 @@
 import { Message } from 'discord.js'
-exports.run = async (_message: Message, _args: Array<string>) => {
+exports.run = (_message: Message, _args: Array<string>) => {
   let messagecount: number = parseInt(_args[0], undefined)
-  if (messagecount <= 1 || messagecount > 100) return _message.channel.send('I can\'t delete this many messages!')
-  await _message.delete()
-  await _message.channel.fetchMessages({ limit: messagecount }).then(messages => { _message.channel.bulkDelete(messages) })
-  await _message.channel.send(`:ok_hand: Deleted ${messagecount} messages`).then(botmsg => botmsg.delete(5000))
+  if (messagecount <= 2 || messagecount >= 100) return _message.channel.send('I can\'t delete this many messages!')
+  else {
+    _message.delete()
+    _message.channel.fetchMessages({ limit: messagecount }).then(messages => { _message.channel.bulkDelete(messages) })
+    _message.channel.send(`:ok_hand: Deleted ${messagecount} messages`).then(botmsg => botmsg.delete(5000))
+  }
 }
 
 exports.GlobalSettings = {
