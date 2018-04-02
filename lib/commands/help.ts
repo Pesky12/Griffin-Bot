793ef29e191d3f8ee12c0fa82374edbc0e07d2a9
Pesky12 <alesoelsov@gmail.com>
2018-04-02 11:12:54 +0200

    Reverted help.ts to 1a53907

--- lib/commands/help.ts
+++ lib/commands/help.ts
@@ -1,39 +1,39 @@
-import { DocumentData } from '@google-cloud/firestore'
-import { resolve } from 'dns'
 
-import { checkCommandPerms, getCommandSettings } from '../Utils/checkAccess'
 import { helpDescEmbed } from '../Utils/embeds'
-import { Client, Message } from 'discord.js'
+import { checkAccess } from '../Utils/checkAccess'
 
-exports.run = async (_message: Message, _args: Array<string>, _client: Client) => {
-  let longest = Array.from(_client.commands.keys()).reduce((long, str) => Math.max(long, str.length), 0)
-  let helpList = await filterList()
-  let command = _client.commands.get(_args[0])
-  if (command) return _message.channel.send({ embed: helpDescEmbed(command) })
-  function filterList () {
-    return new Promise(async (resolve, reject) => {
-      let list: Array<DocumentData>
-      let settings = await getCommandSettings(_message.guild)
-      _client.commands.forEach((data: any) => {
-        if (checkCommandPerms(data, settings.find(cmd.help.name), _message)) list.push(data)
-        resolve(list)
-      })
-    })
+exports.run = async (client, message, args, config) => {
+  let longest = Array.from(client.commands.keys()).reduce((long, str) => Math.max(long, str.length), 0)
+  let helpList = []
+  let command = client.commands.get(args[0])
+  if (command) {
+    if (!command.settings.public || command.settings.owneronly) return
+    return message.channel.send({ embed: helpDescEmbed(command) })
   }
-
-  _message.channel.send(`Here is the list of commands you can use ❤\nTo see all the commands use ${process.env.PREFIX}help all\n${helpList.join('\n')}`, { code: 'css' })
+  let imustpromisecuzjs = client.commands.map(c => {
+    if (args[0] !== 'all') {
+      let access = checkAccess(message, c)
+      if (!access) return
+    } else {
+      if (!c.settings.public || c.settings.owneronly) return
+    }
+    helpList.push(`{ ${process.env.PREFIX}${c.help.name}${' '.repeat(longest - c.help.name.length)} : '${c.help.description}' }`)
+  })
+  await Promise.all(imustpromisecuzjs)
+  message.channel.send(`Here is the list of commands you can use ❤\nTo see all the commands use ${process.env.PREFIX}help all\n${helpList.join('\n')}`, { code: 'css' })
 }
 
-exports.GlobalSettings = {
-  enabled: false,
-  pm: false,
-  name: 'help',
-  shortDesc: '',
-  longDesc: '',
-  usage: ''
-}
-
-exports.GuildDefaultSettings = {
+exports.settings = {
   enabled: true,
-  perms: []
+  public: true,
+  pm: true,
+  owneronly: false,
+  permissionsRequired: []
+}
+
+exports.help = {
+  name: 'help',
+  description: '❔ It will show you this window!!',
+  longDescription: '',
+  usage: 'help [command]'
 }
