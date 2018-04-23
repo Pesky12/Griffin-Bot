a11c3516cf6983b4e2293f62f5ebd92ded934437
Pesky12 <alesoelsov@gmail.com>
2018-04-23 11:48:19 +0200

    Removed unused files

--- mongoTesting.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-let uri = 'mongodb+srv://Chloe:PoZatQSx3Gr0jrIl@chloebot-ii0qt.mongodb.net/test'
-
-MongoClient.connect(uri, (err, client) => {
-  client.db('ChloeBot').collection('Guilds').insertOne({ "lol": Math.random() * Math.PI })
-  client.close()
-})
